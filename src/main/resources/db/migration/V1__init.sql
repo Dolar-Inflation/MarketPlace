6ab0ed06d8abe628ecd7f9f42c313d414abaaf5a
Dolar-Inflation <vandarkholm09@gmail.com>
2026-02-18 13:43:22 +0300

    добавил миграцию бд с нужными связями

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,9 +2,9 @@
 -- PostgreSQL database cluster dump
 --
 
--- Started on 2026-02-12 15:44:50
+-- Started on 2026-02-18 13:41:14
 
-\restrict PEeP7I8VMzgM8cT0lf7Q4Wc7PWluCkuwD7OhfBOgoLPYzcHaKyyt1mDtwcBJfO4
+\restrict lBEAZD8lh26Ub5XMkxEJI0dlud7wuymmMIhm4x3vDIcJUY7t8rsaygmJwD1gE3u
 
 SET default_transaction_read_only = off;
 
@@ -27,9 +27,7 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 
 
 
-
-
-\unrestrict PEeP7I8VMzgM8cT0lf7Q4Wc7PWluCkuwD7OhfBOgoLPYzcHaKyyt1mDtwcBJfO4
+\unrestrict lBEAZD8lh26Ub5XMkxEJI0dlud7wuymmMIhm4x3vDIcJUY7t8rsaygmJwD1gE3u
 
 --
 -- Databases
@@ -45,12 +43,12 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 -- PostgreSQL database dump
 --
 
-\restrict lH1CeG5vAQsMbpRbb12I0tLco1JSOd8UpO3C47FC7ccV1NczkM9fWxHvZnOhz6v
+\restrict 4mkZeBEQQ8aIHoiyGMXjvA5RoAVFN8OIEBPbIiR3bb2vBUrgjv4jcQxZrAk4Nbc
 
 -- Dumped from database version 18.0
 -- Dumped by pg_dump version 18.0
 
--- Started on 2026-02-12 15:44:51
+-- Started on 2026-02-18 13:41:14
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -64,13 +62,13 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
--- Completed on 2026-02-12 15:44:51
+-- Completed on 2026-02-18 13:41:15
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict lH1CeG5vAQsMbpRbb12I0tLco1JSOd8UpO3C47FC7ccV1NczkM9fWxHvZnOhz6v
+\unrestrict 4mkZeBEQQ8aIHoiyGMXjvA5RoAVFN8OIEBPbIiR3bb2vBUrgjv4jcQxZrAk4Nbc
 
 --
 -- Database "marketplacedb" dump
@@ -80,12 +78,12 @@ SET row_security = off;
 -- PostgreSQL database dump
 --
 
-\restrict XOowOLUw7VO3wW9zri9FHzd1iM8QMbMYr9OPh0crONGweRa3lVGPucBx8PUcdJa
+\restrict UOhbXNX7KeJ1VPXrOTWzwZAvD4vroLCDrXgUn4LOpYlUM2WtZB0RWCWKRZ5YEdf
 
 -- Dumped from database version 18.0
 -- Dumped by pg_dump version 18.0
 
--- Started on 2026-02-12 15:44:51
+-- Started on 2026-02-18 13:41:15
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -100,18 +98,16 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 4977 (class 1262 OID 72290)
--- Name: marketplacedb; Type: DATABASE; Schema: -; Owner: postgres
+-- TOC entry 4990 (class 1262 OID 72290)
+-- Name: marketplacedb; Type: DATABASE; Schema: -; Owner: -
 --
 
 CREATE DATABASE marketplacedb WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'Russian_Russia.1251';
 
 
-ALTER DATABASE marketplacedb OWNER TO postgres;
-
-\unrestrict XOowOLUw7VO3wW9zri9FHzd1iM8QMbMYr9OPh0crONGweRa3lVGPucBx8PUcdJa
+\unrestrict UOhbXNX7KeJ1VPXrOTWzwZAvD4vroLCDrXgUn4LOpYlUM2WtZB0RWCWKRZ5YEdf
 \connect marketplacedb
-\restrict XOowOLUw7VO3wW9zri9FHzd1iM8QMbMYr9OPh0crONGweRa3lVGPucBx8PUcdJa
+\restrict UOhbXNX7KeJ1VPXrOTWzwZAvD4vroLCDrXgUn4LOpYlUM2WtZB0RWCWKRZ5YEdf
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -131,21 +127,20 @@ SET default_table_access_method = heap;
 
 --
 -- TOC entry 220 (class 1259 OID 72292)
--- Name: account; Type: TABLE; Schema: public; Owner: postgres
+-- Name: account; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.account (
     id bigint NOT NULL,
-    password character varying NOT NULL,
-    accountname character varying NOT NULL
+    password text NOT NULL,
+    accountname text NOT NULL,
+    acc_order_id bigint
 );
 
 
-ALTER TABLE public.account OWNER TO postgres;
-
 --
 -- TOC entry 219 (class 1259 OID 72291)
--- Name: account_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: account_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 ALTER TABLE public.account ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
@@ -159,21 +154,48 @@ ALTER TABLE public.account ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 223 (class 1259 OID 80491)
--- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 221 (class 1259 OID 80571)
+-- Name: order_product; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.order_product (
+    id bigint NOT NULL,
+    order_id bigint,
+    product_id bigint
+);
+
+
+--
+-- TOC entry 226 (class 1259 OID 80604)
+-- Name: order_product_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+ALTER TABLE public.order_product ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.order_product_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 223 (class 1259 OID 80577)
+-- Name: orders; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.orders (
     id bigint NOT NULL,
-    amountprice money NOT NULL
+    amount character varying,
+    acc_id bigint,
+    product_id bigint
 );
 
 
-ALTER TABLE public.orders OWNER TO postgres;
-
 --
--- TOC entry 222 (class 1259 OID 80490)
--- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- TOC entry 224 (class 1259 OID 80580)
+-- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 ALTER TABLE public.orders ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
@@ -187,19 +209,36 @@ ALTER TABLE public.orders ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 221 (class 1259 OID 80487)
--- Name: products; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 222 (class 1259 OID 80574)
+-- Name: products; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.products (
+    id bigint NOT NULL,
+    product_name character varying NOT NULL,
+    price double precision,
+    order_id bigint
 );
 
 
-ALTER TABLE public.products OWNER TO postgres;
+--
+-- TOC entry 225 (class 1259 OID 80594)
+-- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+ALTER TABLE public.products ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.products_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
 
 --
--- TOC entry 4820 (class 2606 OID 72301)
--- Name: account account_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 4826 (class 2606 OID 72301)
+-- Name: account account_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.account
@@ -207,8 +246,8 @@ ALTER TABLE ONLY public.account
 
 
 --
--- TOC entry 4822 (class 2606 OID 80485)
--- Name: account account_unique; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 4828 (class 2606 OID 80568)
+-- Name: account account_unique; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.account
@@ -216,23 +255,68 @@ ALTER TABLE ONLY public.account
 
 
 --
--- TOC entry 4824 (class 2606 OID 80497)
--- Name: orders orders_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 4830 (class 2606 OID 80610)
+-- Name: order_product order_product_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.order_product
+    ADD CONSTRAINT order_product_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4834 (class 2606 OID 80586)
+-- Name: orders orders_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.orders
     ADD CONSTRAINT orders_pk PRIMARY KEY (id);
 
 
--- Completed on 2026-02-12 15:44:51
+--
+-- TOC entry 4832 (class 2606 OID 80600)
+-- Name: products products_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4835 (class 2606 OID 80589)
+-- Name: account account_orders_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.account
+    ADD CONSTRAINT account_orders_fk FOREIGN KEY (acc_order_id) REFERENCES public.orders(id);
+
+
+--
+-- TOC entry 4836 (class 2606 OID 80611)
+-- Name: order_product order_product_orders_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.order_product
+    ADD CONSTRAINT order_product_orders_fk FOREIGN KEY (order_id) REFERENCES public.orders(id);
+
+
+--
+-- TOC entry 4837 (class 2606 OID 80616)
+-- Name: order_product order_product_products_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.order_product
+    ADD CONSTRAINT order_product_products_fk FOREIGN KEY (product_id) REFERENCES public.products(id);
+
+
+-- Completed on 2026-02-18 13:41:15
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict XOowOLUw7VO3wW9zri9FHzd1iM8QMbMYr9OPh0crONGweRa3lVGPucBx8PUcdJa
+\unrestrict UOhbXNX7KeJ1VPXrOTWzwZAvD4vroLCDrXgUn4LOpYlUM2WtZB0RWCWKRZ5YEdf
 
--- Completed on 2026-02-12 15:44:51
+-- Completed on 2026-02-18 13:41:15
 
 --
 -- PostgreSQL database cluster dump complete
